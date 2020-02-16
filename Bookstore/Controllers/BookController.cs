9d424f60da6c1469a125c44169e0916a65e805d7
John <johnhallden@gmail.com>
2020-02-16 11:01:19 +0100

    Kan läsa data ifrån xml och ska försöka få till resterande crud senare

---
+++ Bookstore/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+
+        public ActionResult Index() //https://www.aspsnippets.com/Articles/Read-and-display-XML-data-in-View-in-ASPNet-MVC-Razor.aspx
+        {
+            List<Book> bookList = new List<Book>();
+
+            XmlDocument doc = new XmlDocument();
+            //doc.Load(@"../XML/Books.xml"); 
+            //doc.Load(@"C:\Users\John\source\repos\Bookstore\Bookstore\XML\Books.xml");
+            doc.Load(Server.MapPath("~/XML/Books.xml"));
+
+            foreach (XmlNode node in doc.SelectNodes("/Books/Book"))
+            {
+                bookList.Add(new Book
+                {
+                    id = int.Parse(node["id"].InnerText),
+                    title = node["title"].InnerText,
+                    author = node["author"].InnerText,
+                    price = int.Parse(node["price"].InnerText),
+                    cover = node["cover"].InnerText,
+                    description = node["description"].InnerText
+                });
+            }
+
+            Console.WriteLine(bookList.Count);
+
+            return View(bookList);
+        }
+    }
+}
---
+++ Bookstore/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public int price { get; set; }
+        public string cover { get; set; }
+        public string description { get; set; }
+    }
+}
